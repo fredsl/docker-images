5702b023573e8faec8298ad7b9c6b5c3d93c14c4
Robson Nascimento <robson@fredbooking.com>
2019-12-02 16:09:17 +1300

    test

--- fred-hardware-php/build.sh
+++ fred-hardware-php/build.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+apt-get install docker-ce
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+docker buildx create --name xbuilder
+docker buildx use xbuilder
+docker buildx inspect --bootstrap
 
 docker build \
      --build-arg ARCH=arm \
@@ -12,11 +20,13 @@ docker build \
      --build-arg PHP_VARIANT=$PHP_VARIANT \
      -t $DOCKER_REPO:$PHP_VERSION-$PHP_VARIANT-aarch64 .
 
-ARCH=amd64
-
 docker build \
      --build-arg ARCH=x86_64 \
      --build-arg PHP_VERSION=$PHP_VERSION \
      --build-arg PHP_VARIANT=$PHP_VARIANT \
      -t $DOCKER_REPO:$PHP_VERSION-$PHP_VARIANT-amd64 \
      -t $DOCKER_REPO:$DOCKER_TAG .
+
+docker push $DOCKER_REPO:$PHP_VERSION-$PHP_VARIANT-armv7hf
+docker push $DOCKER_REPO:$PHP_VERSION-$PHP_VARIANT-aarch64
+docker push $DOCKER_REPO:$PHP_VERSION-$PHP_VARIANT-amd64
